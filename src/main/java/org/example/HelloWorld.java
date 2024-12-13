249569e529aea7b7165b550ccefb214116c5ae6b
Hari Sukumaran <harisukumaran@hotmail.com>
2024-12-13 15:14:11 -0600

    Added comment

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-
+// loop from 1 to 5
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
